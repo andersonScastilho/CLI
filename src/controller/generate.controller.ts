32586271403397b067dcb7ddebdcad7f20d9c2ec
Anderson Leonardo <131002730+SuportePapel@users.noreply.github.com>
2023-05-25 12:41:17 -0300

    adicionando method que clona o repositorio e renomeia a pasta

---
+++ src/controller/generate.controller.ts
@@ -0,0 +1,45 @@
+import shelljs from 'shelljs';
+import fs from 'node:fs';
+import path from 'node:path';
+
+import { IAnswers } from 'interfaces/answer.interface';
+
+import { EChoiceBoilerPlate } from 'enum/choices-boileplate.enum';
+import { EGitName } from 'enum/git-name.enum';
+
+class GenerateController {
+  public gen(answer: IAnswers) {
+    try {
+      switch (answer.tech) {
+        case EChoiceBoilerPlate.NODEJS_TS:
+          this._execPath(EGitName.NODEJS_TS, answer.folderName);
+          break;
+        case EChoiceBoilerPlate.SCSS:
+          this._execPath(EGitName.SCSS, answer.folderName);
+
+          break;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  private _execPath(gitName: string, folderName: string) {
+    try {
+      shelljs.cd(path.resolve());
+      shelljs.exec(
+        `git clone https://github.com/andersonScastilho/${gitName}.git`,
+      );
+
+      fs.renameSync(
+        `${path.join(path.resolve(), gitName)}`,
+        `${path.join(path.resolve(), folderName)}`,
+      );
+
+      console.log('Arquivo criado com sucesso');
+      return shelljs.exit();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+export const GenFile = new GenerateController();
--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ import { questions } from 'questions';
 
 //Interface
 import { IAnswers } from 'interfaces/answer.interface';
+import { GenFile } from 'controller/generate.controller';
 
 class Init {
   constructor() {
     inquirer.prompt(questions).then((answer: IAnswers) => {
-      console.log(answer);
+      GenFile.gen(answer);
     });
   }
 }

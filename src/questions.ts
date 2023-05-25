05453069581de4f722d231c6cd24f09c1cf17405
Anderson Leonardo <131002730+SuportePapel@users.noreply.github.com>
2023-05-25 10:25:38 -0300

    Adicionando validacao para nome da pasta

--- src/questions.ts
+++ src/questions.ts
@@ -9,7 +9,21 @@ export const questions = [
   },
   {
     type: 'input',
-    name: 'techName',
+    name: 'folderName',
     message: 'Qual nome devo dar para pasta do projeto?',
+    validate(folderName: string) {
+      if (!folderName) return 'Insirá um valor para o nome do projeto!';
+
+      if (/[^\w\s-]/.test(folderName))
+        return 'Não pode conter caracteres especiais';
+
+      if (
+        folderName === 'BoilerPlateNodeTypescript' ||
+        folderName === 'BoilerPlateScss'
+      )
+        return 'Não é possivel criar a pasta com esse nome';
+
+      return true;
+    },
   },
 ];
